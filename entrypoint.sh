62391315597044faedc57a36c56005ff887f65a8
Syer10 <syer10@users.noreply.github.com>
2021-05-25 16:22:36 -0400

    Use pushd and popd

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ yum install -y rpm-build
 
 
 mkdir -p /usr/java/openjdk
-cd /usr/java/openjdk
+pushd /usr/java/openjdk
 curl "https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_linux-x64_bin.tar.gz" --output openjdk-15.0.2_linux-x64_bin.tar.gz
 tar -xzvf openjdk-15.0.2_linux-x64_bin.tar.gz
 java -version
-cd ~
-ls
+popd
+pwd
 $1
